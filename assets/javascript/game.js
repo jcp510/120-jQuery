9a9eb00763587d5ab3a75b880c419b6bf5fc73dd
Carlos Ponce <jcponce510@gmail.com>
2019-04-25 16:26:29 -0700

    Add random number assignment to crystals.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,29 @@
 $(document).ready(function() {
-    // Generates random number between 19 - 120.
+    // Generates random number between min - max (max is not inclusive).
     let makeRanNum = function(min, max) {
         return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min))) + min;
     };
-    
     let playerScore = 0;
-
+    let blue, green, violet, red;
+    
     // Initialize game with a random number.  
-    $("#ranNum").text(makeRanNum(19, 121));
+    $("#ran-num").text(makeRanNum(19, 121));
 
     // Initialize player score with value of 0.
-    $("#totalScoreNum").text(playerScore);
-    
-    // Assign random values between 1 -12 to each crystal.
+    $("#tot-score-num").text(playerScore);
+
+    // Assign random values between 1 - 12 to each crystal.
+    blue = makeRanNum(1, 13);
+    console.log("blue = " + blue);
+
+    green = makeRanNum(1, 13);
+    console.log("green = " + green);
+
+    violet = makeRanNum(1, 13);
+    console.log("violet = " + violet);
+
+    red = makeRanNum(1, 13);
+    console.log("red = " + red);
 });
 
 
