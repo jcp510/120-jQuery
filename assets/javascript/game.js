13e172841361aa4edf1b6d1948dae799e8e8e7f1
Carlos Ponce <jcponce510@gmail.com>
2019-04-24 17:57:04 -0700

    Add random number generator.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-
+    // Generate random number between 19 - 120.
+    $("#ranNum").text(function() {
+        return Math.floor(Math.random() * (Math.floor(121) - Math.ceil(19))) + 19;
+    });
 });
