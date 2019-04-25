ac1e832e7339e9dd852d9bec7cae69ee15a47e40
Carlos Ponce <jcponce510@gmail.com>
2019-04-25 12:43:04 -0700

    Add crystal images and player score.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
-    // Generate random number between 19 - 120.
-    $("#ranNum").text(function() {
-        return Math.floor(Math.random() * (Math.floor(121) - Math.ceil(19))) + 19;
-    });
+    // Generates random number between 19 - 120.
+    let makeRanNum = function(min, max) {
+        return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min))) + min;
+    };
+    
+    let playerScore = 0;
+
+    // Initialize game with a random number.  
+    $("#ranNum").text(makeRanNum(19, 121));
+
+    // Initialize player score with value of 0.
+    $("#totalScoreNum").text(playerScore);
+    
+    // Assign random values between 1 -12 to each crystal.
 });
+
+
